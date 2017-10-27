403577045c2a8d8fb0fe1d6657ac2cb2f2f8163c
m.mohaghegh <m.mohaghegh@gmail.com>
2017-10-27 17:41:19 +0200

    The name of the nest distribution changed on the source of the nest.

--- nest/install_2.12.0_linux.sh
+++ nest/install_2.12.0_linux.sh
@@ -4,7 +4,7 @@ cd dist
 ./install-nest-2.12.0.sh
 cd ..
 
-NEST_INSTALL=$(pwd)/dist/install/nest-simulator-2.12.0
+NEST_INSTALL=$(pwd)/dist/install/nest-2.12.0
 
 cd module
 ./install-module-2.12.0.sh "$NEST_INSTALL"
