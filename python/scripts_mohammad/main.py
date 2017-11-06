949d9dd5654af70d2c9a456b18ce35e0df0e7a61
Mohammadreza Mohagheghi <m.mohaghegh@gmail.com>
2017-11-06 15:43:10 +0100

    Ramp implementation in a new format with in the main file Mikael Lindahl has designed.

---
+++ python/scripts_mohammad/main.py
@@ -0,0 +1,194 @@
+# Create by Mikael Lindahl on 4/12/17.
+
+from core.network import structure
+from core.network import engine
+from core import my_nest, data_to_disk
+from core.network.parameters.eneuro import EneuroPar
+
+from core.network.parameters.eneuro_activation import EneuroActivationPar
+from core.network.parameters.eneuro_activation_beta import EneuroActivationBetaPar
+from core.network.parameters.eneuro_sw import EneuroSwPar
+
+from core.network.parameters.eneuro_ramp import EneuroRampPar
+# from core.network.default_params import Beta
+from scripts_inhibition.base_oscillation import add_GI, add_GPe
+import pprint
+import json
+import os
+import sys
+
+import plot
+import randomized_params_plot
+import mean_firing_rates
+import mean_firing_rates_plot
+import list_parameters
+
+pp = pprint.pprint
+
+
+def save_node_random_params(pops, path):
+
+    d = {}
+
+    for node in ['FS', 'GI',  'GF', 'GA', 'M1', 'M2', 'ST', 'SN']:
+
+        d[node] = {}
+
+        for param in ['V_th', 'V_m', 'C_m', 'E_L']:
+
+            d[node][param] = [s[param] for s in my_nest.GetStatus(pops[node].ids)]
+
+
+        d[node]['V_th-E_L']= [a-b for a,b in zip(d[node]['V_th'], d[node]['E_L'])]
+
+
+    if not os.path.isdir(os.path.dirname(path)):
+        data_to_disk.mkdir(os.path.dirname(path))
+
+    json.dump( d, open(path, 'w'))
+
+def build(par):
+    # ******
+    # Build
+    # ******
+    surfs, pops = structure.build(par.get_nest(),
+                                  par.get_surf(),
+                                  par.get_popu())
+
+    return surfs, pops
+
+
+def connect(par, surfs, pops):
+    args = [pops, surfs, par.get_nest(), par.get_conn(), True]
+
+    structure.connect(*args)
+
+
+def postprocessing(pops):
+    d = {}
+
+    signal_type = 'spike_signal'
+
+    d_signals = pops.get(signal_type)
+
+    for name, signal in d_signals.items():
+        engine.fill_duds_node(d, name, signal_type, signal)
+
+    add_GI(d)
+    add_GPe(d)
+
+    return d
+
+
+def main(mode, size):
+    my_nest.ResetKernel()
+
+    # Get parameters
+    # par = EneuroBetaPar(other=EneuroPar())
+
+    if mode in ['activation-control',  'activation-dopamine-depleted']:
+
+        if mode == 'activation-control':
+
+            par = EneuroActivationPar(other=EneuroPar())
+            dop = 0.8
+
+        elif mode == 'activation-dopamine-depleted':
+            par = EneuroActivationBetaPar(other=EneuroPar())
+            dop = 0.0
+
+    elif mode in ['slow-wave-control', 'slow-wave-dopamine-depleted']:
+
+        par = EneuroSwPar(other=EneuroPar())
+
+        if mode == 'slow-wave-control':
+            dop = 0.8
+        elif mode == 'slow-wave-dopamine-depleted':
+            dop = 0.0
+
+    base = os.path.join(os.getenv('BGMODEL_HOME'), 'results/example/eneuro', str(size), mode )
+
+    # Configure simulation parameters
+    par.set({
+        'simu': {
+            'local_num_threads': 8,
+            'path_data': base+'/data',
+            'path_figure': base+'/fig',
+            'path_nest': base+'/nest/',  # trailing slash important
+            'stop_rec': 10000.,
+            'sim_stop': 10000.,
+            'print_time': True,
+            'sd_params': {
+                'to_file': True,
+                'to_memory': False
+            },
+
+        },
+        'netw': {
+            'tata_dop':dop,
+            'size': size
+        },
+        # 'node': {
+        #     'C1': {'rate': 546.},
+        #     'C2': {'rate': 722.},
+        #     'CF': {'rate': 787.},
+        #     'CS': {'rate': 250.},
+        #     'ES': {'rate': 1530.}
+        # },
+    })
+
+    par.nest_set_kernel_status()
+
+    # Save parametesr
+    list_parameters.main(base, par)
+
+    # Clear nest data directory
+    par.nest_clear_data_path({'display': True})
+
+    # Show kernel status
+    pp(my_nest.GetKernelStatus())
+
+    # Create news populations and connections structures
+    surfs, pops = build(par)
+
+    #Example getting C1 nest ids
+    # >> pops['C1'].ids
+
+    save_node_random_params(pops,base+'/randomized-params.json')
+
+    # print(pops)
+
+    # Connect populations accordingly to connections structure
+    connect(par, surfs, pops)
+    #
+    # # Simulate
+    my_nest.Simulate(3000.)
+    #
+    # # Create spike signals
+    d = postprocessing(pops)
+    #
+    # # Save
+    sd = data_to_disk.Storage_dic.load(par.dic['simu']['path_data'], ['Net_0'])
+    sd.save_dic({'Net_0': d}, **{'use_hash': False})
+
+
+# main()
+if __name__ == '__main__':
+
+    size = sys.argv[1] if len(sys.argv)>1 else 3000
+
+    modes = [
+        'activation-control',
+        'activation-dopamine-depleted',
+        'slow-wave-control',
+        'slow-wave-dopamine-depleted'
+    ]
+
+    for mode in modes:
+        main(mode, size)
+
+        plot.main(mode, size)
+
+        randomized_params_plot.main(mode, size)
+        mean_firing_rates.main(mode, size)
+        mean_firing_rates_plot.main(mode, size)
