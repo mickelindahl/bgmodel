fe4cf55f635ad0c24ea0f733fb769724c20b8dec
Mohammadreza Mohagheghi <m.mohaghegh@gmail.com>
2017-12-11 16:18:42 +0100

    This function is called in the data_concat_save_as_mat. for the concatenation of the data.

---
+++ python/scripts_mohammad/readgdf.m
@@ -0,0 +1,21 @@
+%% Created by Mohagheghi 09.11.17
+% This function reads gdf files
+% Maximum value that uint16 can store is 65535 and the maximum value that
+% uint32 can store is around 400000s with 0.1 ms resolution. Then becareful
+% about using the right data format!
+
+function [ids,spk_times] = readgdf(fl_name,sim_res)
+    
+    data = importdata(fl_name);
+    
+    if max(data(:,1)) > intmax('uint16')
+        ids = uint32(data(:,1));
+    else
+        ids = uint16(data(:,1));
+    end
+    
+    if ceil(max(data(:,2))/sim_res) > intmax('uint32')
+        spk_times = uint64(data(:,2)/sim_res);
+    else
+        spk_times = uint32(data(:,2)/sim_res);
+    end
