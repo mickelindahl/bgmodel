6c9845c050c2477608bfdb69e94ed9bf18f68a9d
Mohammadreza Mohagheghi <m.mohaghegh@gmail.com>
2018-03-25 20:38:03 +0200

    Bug fixed and tested for running on cluster.

--- python/scripts_mohammad/data_concat_save_as_mat_sep_stim.m
+++ python/scripts_mohammad/data_concat_save_as_mat_sep_stim.m
@@ -11,7 +11,11 @@ nuclei = {'FS','GA','GF','GI','M1','M2','SN','ST'};
 
 if length(varargin) >= 2
     data_dir = varargin{1};
-    res = str2double(varargin{2});
+    if isstr(varargin{2})
+        res = str2double(varargin{2});
+    else
+        res = varargin{2};
+    end
 else
     data_dir = ['/Users/Mohammad/Documents/PhD/Projects/BGmodel/bgmodel',...
         '/results/example/eneuro/3000/activation-control/',...
@@ -24,30 +28,30 @@ end
 
 % Initialization of empty vars
 % res = 0.1;           %ms
-mat_data_dir = [data_dir,'/','mat_data/'];
 
-if exist(mat_data_dir,'dir') ~= 7
-    mkdir(mat_data_dir)
-end
-
+mat_datadir = data_dir;
 % nestdir = [data_dir,'nest/'];
 % 
 % stimdata = load([data_dir,'stimspec']);
 % 
 % ref_time = stimdata.STRramp.stop_times;
 % 
-all_dirs_data = load('dirdata.mat');
+all_dirs_data = load([data_dir,'dir-data.mat']);
 all_dirs = all_dirs_data.dirs;
-
+Gs = [];
 
 for nc_ind = 1:length(nuclei)
     
-    for dir_ind = 2:size(all_dirs,1)
+    for dir_ind = 1:size(all_dirs,1)
         nestdir = all_dirs(dir_ind,:);
         
         data_dir = strsplit(nestdir,'nest');
         data_dir = data_dir{1};
         stimdata = load([data_dir,'stimspec']);
+        
+        cond_GA_MSN_data = load([data_dir,'modifiedweights']);
+        GA_M1 = (cond_GA_MSN_data.GA_M1.max + cond_GA_MSN_data.GA_M1.min)/2;
+        Gs = [Gs,GA_M1];
 
         ref_time = stimdata.STRramp.stop_times;
 
@@ -65,10 +69,50 @@ for nc_ind = 1:length(nuclei)
             spk_times = [spk_times;spks];
         end
 
-        data{nc_ind} = struct(nuclei{nc_ind},struct('spktimes',spk_times,'N_ids',N_ids));
+        data{nc_ind,dir_ind} = struct(nuclei{nc_ind},struct('spktimes',spk_times,'N_ids',N_ids),...
+                                'rates',stimdata.STRramp.rates,...
+                                'timerefs',struct('GPAstim',stimdata.GPAstop.start_times,...
+                                                  'STRstim',stimdata.STRramp.stop_times,...
+                                                  'STNstim',stimdata.STNstop.start_times),...
+                                'weight',GA_M1);
     end
 %     save([mat_data_dir,nuclei{nc_ind},'-spikedata'],'N_ids','spk_times','res')
     
 end
 
-exit
+% mat_data_dir = [data_dir,'/','mat_data/'];
+str_tmp = strsplit(data_dir,'tr');
+str_tmp = str_tmp{end};
+% str_tmp = strsplit(str_tmp{1},'tr');
+
+
+stim_props = data{1}.rates;
+
+mat_datadir = [mat_datadir,'B-'];
+for ind = 1:length(stim_props)
+    mat_datadir = [mat_datadir,num2str(stim_props(ind))];
+end
+
+stim_props = data{end}.rates;
+
+mat_datadir = [mat_datadir,'-E-'];
+for ind = 1:length(stim_props)
+    mat_datadir = [mat_datadir,num2str(stim_props(ind))];
+end
+
+if exist(mat_datadir,'dir') ~= 7
+    mkdir(mat_datadir)
+end
+
+if length(unique(Gs)) == 1
+    mat_fl_name = [mat_datadir,'/W',num2str(unique(Gs*100),'%i'),'-tr',str_tmp(1:end-1)];
+    disp(['All weights are equal. They are store in: ',mat_fl_name])
+else
+    mat_fl_name = [mat_datadir,'/tr',str_tmp(1:end-1)];
+    disp(['All weights are NOT equal. They are store in: ',mat_fl_name])
+end
+
+save(mat_fl_name,'data','nuclei')
+
+% exit
+% pause()
