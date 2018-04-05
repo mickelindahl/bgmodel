855e25dab42d0d6b55095010536eb94bc818e584
Mohammadreza Mohagheghi <m.mohaghegh@gmail.com>
2018-04-05 08:07:53 +0200

    After making a mistake leading to changing main.py file on gonzo to previous revision, all changes are applied in this commit.

--- matlab/Analysis-scripts/Multiplestim-STRSTNGPA/main.m
+++ matlab/Analysis-scripts/Multiplestim-STRSTNGPA/main.m
@@ -5,14 +5,36 @@
 % condition where we can make sure that the network state for each
 % stimulation is similar.
 
-function [] = main(data_path,data_dir,weights,numtrs)
+function [] = main(data_path,weights,numtrs)
     compress_flag = true;
-    if exist([data_path,'/all_proc_data.mat'],'file') ~= 2
+    data_dir = directory_extract(data_path);
+    if exist(fullfile(data_path,'all_proc_data.mat'),'file') ~= 2
         for dir_ind = 1:length(data_dir)
 
             [st_par,nctr,avgfr,...
              avgfrnoov,offt,stimISI,...
-             nctrISI,nc] = main_postproc([data_path,'/',data_dir{dir_ind}],weights,numtrs,compress_flag);
+             nctrISI,nc,t_sample_noov] = main_postproc(fullfile(data_path,data_dir{dir_ind}),weights,numtrs,compress_flag);
+
+            procdata{dir_ind} = struct('stim_param',st_par,...
+                                       'nuclei_trials',nctr,...
+                                       'average_fr',avgfr,...
+                                       'average_fr_no_overlap',avgfrnoov,...
+                                       'offtime',offt,...
+                                       'stim_param_ISI',stimISI,...
+                                       'nuclei_trials_ISI',nctrISI,...
+                                       'time_vec',t_sample_noov,...
+                                       'nuclei',struct('nc_names',nc),...
+                                       'data_dir',fullfile(data_path,'/',data_dir{dir_ind}));
+
+        end
+        save(fullfile(data_path,'all_proc_data'),'procdata','nc')
+    else
+%         indir = dir(data_path);
+        for dir_ind = 1:length(data_dir)
+%             mat_flname = '/procdata_avg_ISI.mat';
+            [st_par,nctr,avgfr,...
+             avgfrnoov,offt,stimISI,...
+             nctrISI,nc] = main_postproc(fullfile(data_path,data_dir{dir_ind}),weights,numtrs,compress_flag);
 
             procdata{dir_ind} = struct('stim_param',st_par,...
                                        'nuclei_trials',nctr,...
@@ -22,144 +44,129 @@ function [] = main(data_path,data_dir,weights,numtrs)
                                        'stim_param_ISI',stimISI,...
                                        'nuclei_trials_ISI',nctrISI,...
                                        'nuclei',struct('nc_names',nc),...
-                                       'data_dir',[data_path,'/',data_dir{dir_ind}]);
-
+                                       'data_dir',fullfile(data_path,data_dir{dir_ind}));
         end
-        save([data_path,'all_proc_data'],'procdata')
-    else
-        indir = dir(data_path);
-        for indir_ind = 1:length(indir)
-            name_str = indir(indir_ind).name;
-            mat_flname = '/procdata_avg_ISI.mat';
-            logical_state = (indir(indir_ind).isdir && ~strcmpi(name_str,'.') && ~strcmpi(name_str,'..')) && ...
-                             exist([data_path,name_str,mat_flname],'file') ~= 2;
-            if logical_state
-                [st_par,nctr,avgfr,...
-                 avgfrnoov,offt,stimISI,...
-                 nctrISI,nc] = main_postproc([data_path,'/',data_dir{dir_ind}],weights,numtrs,compress_flag);
-
-                procdata{dir_ind} = struct('stim_param',st_par,...
-                                           'nuclei_trials',nctr,...
-                                           'average_fr',avgfr,...
-                                           'average_fr_no_overlap',avgfrnoov,...
-                                           'offtime',offt,...
-                                           'stim_param_ISI',stimISI,...
-                                           'nuclei_trials_ISI',nctrISI,...
-                                           'nuclei',struct('nc_names',nc),...
-                                           'data_dir',[data_path,'/',data_dir{dir_ind}]);
-            end
-        end 
+        save(fullfile(data_path,'all_proc_data'),'procdata')
     end
 end
 
 function [stim_pars,nc_trs,avg_frs,...
           avg_frs_no_ov,off_time,...
           stim_pars_ISI,nc_trs_ISI,...
-          nuclei] = main_postproc(data_dir, weights, numtrs, comp_flag)
-    
-    stim_pars = [];
-    nc_trs = [];
-    avg_frs = [];
-    avg_frs_no_ov = [];
-    off_time = [];
-    stim_pars_ISI = [];
-    nc_trs_ISI = [];
-    
-
-    for w_ind = 1:length(weights)
+          nuclei,t_samples_no_ov] = main_postproc(data_dir, weights, numtrs, comp_flag)
+    file_path = fullfile(data_dir,'procdata_avg_ISI.mat');
+    if exist(file_path,'file') ~= 2
+      
+        stim_pars = [];
+        nc_trs = [];
+        avg_frs = [];
+        avg_frs_no_ov = [];
+        off_time = [];
+        stim_pars_ISI = [];
+        nc_trs_ISI = [];
         
-        for tr_ind = 1:numtrs
-            
-            data = load([data_dir,'W',num2str(weights(w_ind)*100,2),...
-                                 '-tr',num2str(tr_ind,2)]);
-                             
-            nuclei = data.nuclei;
-            
-            data = data.data;
-            
-            for nc_ind = 1:length(nuclei)
-                
-                for st_ind = 1:size(data,2)
-                    
-                    disp(['w',num2str(w_ind),'-st',num2str(st_ind),'-nc',num2str(nc_ind),'-tr',num2str(tr_ind)])
-                    
-                    spk_data = getfield(data{nc_ind,st_ind},nuclei{nc_ind});
-                    spktimes = double(spk_data.spktimes)/10;
-                    
-                    if ~isempty(spktimes)
-                        N_ids = double(spk_data.N_ids);
+        % Averaging window 
 
-                        stim_pars = [stim_pars;data{nc_ind,st_ind}.rates];
-                        nc_trs = [nc_trs;[nc_ind,tr_ind]];
+        win_width = 10; %ms
+        overlap = 1;    %ms
 
-                        reftime = data{nc_ind,st_ind}.timerefs.STRstim;
+        win_width_no_ov = 1;    %ms
 
-                        % Averaging window 
+        % Averaging start and end times
 
-                        win_width = 10; %ms
-                        overlap = 1;    %ms
+        avg_st = -500;
+        avg_end = 500;
 
-                        win_width_no_ov = 1;    %ms
+        
+        t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
 
-                        % Averaging start and end times
 
-                        avg_st = -500;
-                        avg_end = 500;
+        for w_ind = 1:length(weights)
 
-                        % Average firing rate of single trials
-                        
-                        if comp_flag
-                            
-                            t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
-                            cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
-                                                             avg_st,avg_end);
+            for tr_ind = 1:numtrs
 
-                            avg_frs_no_ov = [avg_frs_no_ov;uint8(cnttmp)];
-                            
-                        else
+                data = load(fullfile(data_dir,['W',num2str(weights(w_ind)*100,2),...
+                                     '-tr',num2str(tr_ind,2)]));
 
-                            t_samples = (avg_st + win_width/2):overlap:(avg_end - win_width/2);
-                            cnttmp = average_firingrate(spktimes,reftime,N_ids,win_width,...
-                                                       overlap,avg_st,avg_end);
-                            avg_frs = [avg_frs;cnttmp];
+                nuclei = data.nuclei;
 
-                            t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
-                            cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
-                                                             avg_st,avg_end);
+                data = data.data;
 
-                            avg_frs_no_ov = [avg_frs_no_ov;cnttmp];
-                            
+                parfor nc_ind = 1:length(nuclei)
+
+                    for st_ind = 1:size(data,2)
+
+%                         disp(['w',num2str(w_ind),'-st',num2str(st_ind),'-nc',num2str(nc_ind),'-tr',num2str(tr_ind)])
+
+                        spk_data = getfield(data{nc_ind,st_ind},nuclei{nc_ind});
+                        spktimes = double(spk_data.spktimes)/10;
+
+                        if ~isempty(spktimes)
+                            N_ids = double(spk_data.N_ids);
+
+                            stim_pars = [stim_pars;data{nc_ind,st_ind}.rates];
+                            nc_trs = [nc_trs;[nc_ind,tr_ind,weights(w_ind)]];
+
+                            reftime = data{nc_ind,st_ind}.timerefs.STRstim;
+
+                            % Average firing rate of single trials
+
+                            if comp_flag
+
+                                t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
+                                cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
+                                                                 avg_st,avg_end);
+
+                                avg_frs_no_ov = [avg_frs_no_ov;uint8(cnttmp)];
+
+                            else
+
+                                t_samples = (avg_st + win_width/2):overlap:(avg_end - win_width/2);
+                                cnttmp = average_firingrate(spktimes,reftime,N_ids,win_width,...
+                                                           overlap,avg_st,avg_end);
+                                avg_frs = [avg_frs;cnttmp];
+
+                                
+                                cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
+                                                                 avg_st,avg_end);
+
+                                avg_frs_no_ov = [avg_frs_no_ov;cnttmp];
+
+                            end
+
+                            if strcmpi(nuclei{nc_ind},'SN')
+
+                                % Window width for ISI
+
+                                disinh_w = 20;  % ms
+
+                                off_time_tmp = disinh_time_ISI(spktimes,disinh_w,reftime);
+
+                                off_time = [off_time;off_time_tmp];
+
+                                stim_pars_ISI = [stim_pars_ISI;data{nc_ind,st_ind}.rates];
+                                nc_trs_ISI = [nc_trs_ISI;[nc_ind,tr_ind,weights(w_ind)]];
+
+                            end
                         end
 
-                        if strcmpi(nuclei{nc_ind},'SN')
-
-                            % Window width for ISI
-
-                            disinh_w = 20;  % ms
-
-                            off_time_tmp = disinh_time_ISI(spktimes,disinh_w,reftime);
-
-                            off_time = [off_time;off_time_tmp];
-
-                            stim_pars_ISI = [stim_pars_ISI;data{nc_ind,st_ind}.rates];
-                            nc_trs_ISI = [nc_trs_ISI;[nc_ind,tr_ind]];
-
-                        end
                     end
-                    
+    %                 data_nc = data
                 end
-%                 data_nc = data
+
             end
-            
+
         end
-        
+
+        disp('finished')
+        save(file_path,'off_time','stim_pars_ISI','nc_trs_ISI',...
+                                           'avg_frs','stim_pars','nc_trs',...
+                                           'avg_frs_no_ov','t_samples_no_ov',...
+                                           'nuclei')
+                                       
+    else
+        load(file_path)
     end
-    
-    disp('finished')
-    save([data_dir,'procdata_avg_ISI'],'off_time','stim_pars_ISI','nc_trs_ISI',...
-                                       'avg_frs','stim_pars','nc_trs','t_samples',...
-                                       'avg_frs_no_ov','t_samples_no_ov',...
-                                       'nuclei')
 
 end
 
@@ -191,5 +198,17 @@ function no_fr_time = disinh_time_ISI(spk_times,disinh_width,reftime)
         no_fr_time = NaN;
     else
         no_fr_time = spk_times_sort(no_fr) - reftime;
+    end
+end
+
+function dirs = directory_extract(data_path)
+    indir = dir(data_path);
+    cnt_tmp = 0;
+    for ind = 1:length(indir)
+        name_str = indir(ind).name;
+        if indir(ind).isdir && ~strcmpi(name_str,'.') && ~strcmpi(name_str,'..')
+            cnt_tmp = cnt_tmp + 1;
+            dirs{cnt_tmp} = name_str;
+        end
     end
 end
