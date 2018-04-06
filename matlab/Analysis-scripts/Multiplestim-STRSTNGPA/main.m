27aca3e88fa7b209d40ae9652c4643a9df2d891a
m.mohaghegh <m.mohaghegh@gmail.com>
2018-04-06 09:08:02 +0200

    Syncing matlab files with the one in repository done on Mac

--- matlab/Analysis-scripts/Multiplestim-STRSTNGPA/main.m
+++ matlab/Analysis-scripts/Multiplestim-STRSTNGPA/main.m
@@ -26,26 +26,6 @@ function [] = main(data_path,weights,numtrs)
                                        'nuclei',struct('nc_names',nc),...
                                        'data_dir',fullfile(data_path,'/',data_dir{dir_ind}));
 
-<<<<<<< HEAD
-    for dir_ind = 1:length(data_dir)
-        
-        [st_par,nctr,avgfr,...
-         avgfrnoov,offt,stimISI,...
-         nctrISI,nc] = main_postproc([data_path,'/',data_dir{dir_ind}],weights,numtrs);
-     
-        procdata{dir_ind} = struct('stim_param',st_par,...
-                                   'nuclei_trials',nctr,...
-                                   'average_fr',avgfr,...
-                                   'average_fr_no_overlap',avgfrnoov,...
-                                   'offtime',offt,...
-                                   'stim_param_ISI',stimISI,...
-                                   'nuclei_trials_ISI',nctrISI,...
-                                   'nuclei',struct('nc_names',nc),...
-                                   'data_dir',[data_path,'/',data_dir{dir_ind}]);
-    
-    end
-    save([data_path,'/all_proc_data'],'procdata','-v7.3')
-=======
         end
         save(fullfile(data_path,'all_proc_data'),'procdata','nc')
     else
@@ -68,63 +48,11 @@ function [] = main(data_path,weights,numtrs)
         end
         save(fullfile(data_path,'all_proc_data'),'procdata')
     end
->>>>>>> cb501857ce49af1432da7228dcc0701477a10c06
 end
 
 function [stim_pars,nc_trs,avg_frs,...
           avg_frs_no_ov,off_time,...
           stim_pars_ISI,nc_trs_ISI,...
-<<<<<<< HEAD
-          nuclei] = main_postproc(data_dir, weights, numtrs)
-    
-    stim_pars = [];
-    nc_trs = [];
-    avg_frs = [];
-    avg_frs_no_ov = [];
-    off_time = [];
-    stim_pars_ISI = [];
-    nc_trs_ISI = [];
-    
-    % Averaging window 
-
-    win_width = 10; %ms
-    overlap = 1;    %ms
-
-    win_width_no_ov = 1;    %ms
-
-    % Averaging start and end times
-
-    avg_st = -500;
-    avg_end = 500;
-    
-    t_samples = (avg_st + win_width/2):overlap:(avg_end - win_width/2);
-    t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
-    
-
-    for w_ind = 1:length(weights)
-        disp(['W = ',num2str(weights(w_ind))])
-        tic
-        for tr_ind = 1:numtrs
-            
-            data = load([data_dir,'W',num2str(weights(w_ind)*100,2),...
-                                 '-tr',num2str(tr_ind,2)]);
-                             
-            nuclei = data.nuclei;
-            
-            data = data.data;
-            
-            parfor nc_ind = 1:length(nuclei)
-                
-                for st_ind = 1:size(data,2)
-                    
-                    %disp(['w',num2str(w_ind),'-st',num2str(st_ind),'-nc',num2str(nc_ind),'-tr',num2str(tr_ind)])
-                    
-                    spk_data = getfield(data{nc_ind,st_ind},nuclei{nc_ind});
-                    spktimes = double(spk_data.spktimes)/10;
-                    
-                    if ~isempty(spktimes)
-                        N_ids = double(spk_data.N_ids);
-=======
           nuclei,t_samples_no_ov] = main_postproc(data_dir, weights, numtrs, comp_flag)
     file_path = fullfile(data_dir,'procdata_avg_ISI.mat');
     if exist(file_path,'file') ~= 2
@@ -161,24 +89,11 @@ function [stim_pars,nc_trs,avg_frs,...
                                      '-tr',num2str(tr_ind,2)]));
 
                 nuclei = data.nuclei;
->>>>>>> cb501857ce49af1432da7228dcc0701477a10c06
 
                 data = data.data;
 
                 parfor nc_ind = 1:length(nuclei)
 
-<<<<<<< HEAD
-                        % Average firing rate of single trials
-
-                        
-                        cnttmp = average_firingrate(spktimes,reftime,N_ids,win_width,...
-                                                   overlap,avg_st,avg_end);
-                        avg_frs = [avg_frs;cnttmp];
-
-                        
-                        cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
-                                                         avg_st,avg_end);
-=======
                     for st_ind = 1:size(data,2)
 
 %                         disp(['w',num2str(w_ind),'-st',num2str(st_ind),'-nc',num2str(nc_ind),'-tr',num2str(tr_ind)])
@@ -197,7 +112,6 @@ function [stim_pars,nc_trs,avg_frs,...
                             % Average firing rate of single trials
 
                             if comp_flag
->>>>>>> cb501857ce49af1432da7228dcc0701477a10c06
 
                                 t_samples_no_ov = (avg_st + win_width_no_ov/2):(avg_end - win_width_no_ov/2);
                                 cnttmp = average_firingrate_hist(spktimes,reftime,N_ids,win_width_no_ov,...
@@ -241,19 +155,6 @@ function [stim_pars,nc_trs,avg_frs,...
                 end
 
             end
-<<<<<<< HEAD
-            
-            
-        end
-        toc
-    end
-    
-    disp('finished')
-    save([data_dir,'procdata_avg_ISI'],'off_time','stim_pars_ISI','nc_trs_ISI',...
-                                       'avg_frs','stim_pars','nc_trs','t_samples',...
-                                       'avg_frs_no_ov','t_samples_no_ov',...
-                                       'nuclei','-v7.3')
-=======
 
         end
 
@@ -266,7 +167,6 @@ function [stim_pars,nc_trs,avg_frs,...
     else
         load(file_path)
     end
->>>>>>> cb501857ce49af1432da7228dcc0701477a10c06
 
 end
 
@@ -300,8 +200,6 @@ function no_fr_time = disinh_time_ISI(spk_times,disinh_width,reftime)
         no_fr_time = spk_times_sort(no_fr) - reftime;
     end
 end
-<<<<<<< HEAD
-=======
 
 function dirs = directory_extract(data_path)
     indir = dir(data_path);
@@ -314,4 +212,3 @@ function dirs = directory_extract(data_path)
         end
     end
 end
->>>>>>> cb501857ce49af1432da7228dcc0701477a10c06
